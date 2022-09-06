37bb7d9f2b70f152d57a17d632c458e0793f0d99
Hoosang Lee <lhs223@gmail.com>
2022-09-07 08:35:51 +0900

    merge with hs

--- src/arm_state_publisher.cpp
+++ src/arm_state_publisher.cpp
@@ -32,32 +32,34 @@ int main(int argc, char** argv)
   Move_Group_Interface mgi("arm", prefix);
   double timeout = 0.1;
 
-  double m_index;
-  Eigen::MatrixXcd eigen_values;
-  Eigen::MatrixXcd eigen_vectors;
+  
+  
 
   // Loop
   clock_t start, end;
   double result, manipulability;
-  std_msgs::Float64MultiArray ik_result_msg, e_values_msg;
-  ros::Rate loop_rate(250);
+  
+  ros::Rate loop_rate(1000);
   start = clock(); // 측정 시작
   while (ros::ok()){
     
-    mgi.kinematic_state->setJointGroupPositions(mgi.joint_model_group, mgi.current_joint_values);
+    mgi.kinematic_state->setJointGroupPositions(mgi.joint_model_group, mgi.current_joint_position);
     mgi.kinematic_state->enforceBounds(); // Make sure all state variables are within bounds and normalized.
 
     // check self collision
-    std_msgs::Bool self_collision_msg;
-    self_collision_msg.data = mgi.check_self_collision();
-    mgi.self_collision_pub.publish(self_collision_msg);
+    mgi.publish_self_collision();
 
-    // calculate manipulabilty index
-    mgi.kinematic_state->update(); // https://github.com/ros-planning/moveit/pull/188
-    mgi.metrics.getManipulabilityIndex(*mgi.kinematic_state, mgi.joint_model_group, m_index);
-    std_msgs::Float64 m_index_msg;
-    m_index_msg.data = m_index;
-    mgi.m_index_pub.publish(m_index_msg);
+    // publish manipulabilty index
+    mgi.publish_m_index();
+
+    // publish manipulabilty ellipsoid
+    mgi.publish_e_values();
+
+    // publish task velocity
+    mgi.publish_task_velocity();
+
+    // publish current pose
+    mgi.publish_current_pose_rpy();
 
     // // calculate manipulabilty
     // move_group_interface.kinematic_state->update(); // https://github.com/ros-planning/moveit/pull/188
@@ -65,17 +67,6 @@ int main(int argc, char** argv)
     // std_msgs::Float64 m_index_msg;
     // m_index_msg.data = manipulability;
     // move_group_interface.m_index_pub.publish(m_index_msg);
-
-    // calculate manipulabilty ellipsoid
-    mgi.kinematic_state->update(); // https://github.com/ros-planning/moveit/pull/188
-    mgi.metrics.getManipulabilityEllipsoid(*mgi.kinematic_state, mgi.joint_model_group, eigen_values, eigen_vectors);
-    e_values_msg.data.clear();
-    e_values_msg.data.push_back(eigen_values(0).real());
-    e_values_msg.data.push_back(eigen_values(1).real());
-    e_values_msg.data.push_back(eigen_values(2).real());
-    mgi.eigen_value_pub.publish(e_values_msg);
-    // ROS_INFO_STREAM("e_value1: \n" << eigen_values(0) << "\n");  
-
 
     loop_rate.sleep();
     // end = clock(); // 측정 끝
