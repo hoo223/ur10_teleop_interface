c7d3de5d25c5fdf6dfc97e38fc4b9753b481581c
yhkim0566 <63280566@hanmail.net>
2023-07-18 17:19:36 +0900

    markdown edit

--- README.md
+++ README.md
@@ -8,6 +8,17 @@ teleoperation package for ur10 manipulator
 * real_teleop.launch
 * teleop_interface.launch
 
+## PROGP demo initialisation
+* roscore
+* ur10 - ur10_tcp
+* core - real_bringup
+* ce - real_teleop
+* realsense
+* cam - roslaunch aruco_ros double.launch
+* came - rosrun aruco_ros tf_connector_double_promp.py
+* ce - roslaunch ur10_teleop_interface progp_demo.launch
+* rosparam set /real/mode 3 (for joint control)
+
 ## Scripts
 * 
 
--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -8,7 +8,7 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/hmci/test_folder/hsa_project/docker_containers/core_container/share/catkin_ws/src/ur10_teleop_interface/build/devel:$CMAKE_PREFIX_PATH"
-export PATH="/opt/ros/melodic/bin:$PATH"
+export PATH='/opt/ros/melodic/bin:/home/hmci/.local/bin:/home/hmci/anaconda3/condabin:/home/hmci/anaconda3/bin:/home/hmci/.gems/bin:/usr/local/cuda-11.4/bin:/home/hmci/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/hmci/.dotnet/tools:/home/hmci/.dotnet/tools'
 export PWD='/home/hmci/test_folder/hsa_project/docker_containers/core_container/share/catkin_ws/src/ur10_teleop_interface/build'
 export PYTHONPATH='/home/hmci/ros_ws/devel/lib/python2.7/dist-packages:/home/hmci/catkin_ws/devel/lib/python2.7/dist-packages:/opt/ros/melodic/lib/python2.7/dist-packages:/opt/caffe/python'
 export ROSLISP_PACKAGE_DIRECTORIES="/home/hmci/test_folder/hsa_project/docker_containers/core_container/share/catkin_ws/src/ur10_teleop_interface/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
